136ffc246128433d76c57754646382ffe44eed21
benjaminluan <benjaminluan1@gmail.com>
2026-02-09 16:07:42 -0500

    Finished

--- server/server.js
+++ server/server.js
@@ -2,13 +2,19 @@ import express from "express";
 import cors from "cors";
 import records from "./routes/record.js"
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5051;
 const app = express();
+
 
 app.use(cors());
 app.use(express.json());
 app.use("/record", records);
+app.get("/", (req, res) => {
+    res.send("Server is running!");
+});
 
 app.listen(port, () => {
     console.log(`Server Listening on port ${port}`)
-})
+}).on('error', (err) => {
+    console.error('Server error:', err);
+});
